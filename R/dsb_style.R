fc402089bac75e7192d6adb4e262a6467dd456dd
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-09-24 15:48:26 +0200

    edits

--- R/dsb_style.R
+++ R/dsb_style.R
@@ -3,6 +3,7 @@
 #' This function adds a DSB compliant theme to ggplot figures. It can be added at the end of your ggplot syntax.
 #' @param legend a logical value indicating whether a legend is included. Defaults to TRUE.
 #' @return A CVI compliant plot
+#' @author Jeppe Vierø
 #' @export
 
 dsb_style <- function(legend = TRUE) {
@@ -11,17 +12,31 @@ dsb_style <- function(legend = TRUE) {
 
     ggplot2::theme_minimal() +
       ggplot2::theme(legend.position = "none",
-                     axis.text = element_text(colour = "#0F283C",
-                                              face = "bold"),
+                     axis.text.x = element_text(colour = "#0F283C",
+                                                face = "bold",
+                                                size = 12),
+                     axis.text.y = element_text(colour = "#0F283C",
+                                                face = "bold",
+                                                size = 12),
                      axis.title.x = element_text(colour = "#0F283C",
                                                  face = "bold",
+                                                 size = 12,
+                                                 vjust = -1.5),
+                     axis.title.y = element_text(colour = "#0F283C",
+                                                 face = "bold",
+                                                 size = 12,
                                                  vjust = -1.5),
                      strip.text = element_text(colour = "#0F283C",
-                                               face = "bold"),
+                                               face = "bold",
+                                               size = 12),
                      strip.background = element_rect(color = "#0F283C",
                                                      fill = "#E8E1D5"),
-                     title = element_text(colour = "#0F283C",
-                                          face = "bold")
+                     plot.title = element_text(colour = "#0F283C",
+                                               face = "bold",
+                                               size = 16),
+                     plot.subtitle = element_text(colour = "#0F283C",
+                                                  face = "bold",
+                                                  size = 16)
                      )
 
   } else if (legend == TRUE) {
