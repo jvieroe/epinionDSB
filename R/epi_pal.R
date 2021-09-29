780cc78704f5256c6ec7c45ad323e959f36f011a
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-09-29 17:07:33 +0200

    pal

---
+++ R/epi_pal.R
@@ -0,0 +1,19 @@
+#' Use an Epinion styled continuous color palette for your ggplot2 plots
+#'
+#' This function creates a DSB compliant color palette
+#' @param ... x
+#' @return ... z
+#' @author Jeppe Vierø
+#' @export
+
+epi_pal <- function(palette = "epinion",
+                    reverse = FALSE,
+                    ...) {
+
+  pal <- dsb_palettes[[palette]]
+
+  if (reverse) pal <- rev(pal)
+
+  colorRampPalette(pal, ...)
+
+}
