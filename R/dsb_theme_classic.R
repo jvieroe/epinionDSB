ba1e07a9a4c8f729e8270375b867f14d8cf6b1ce
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-10-28 14:59:29 +0200

    namespace

--- R/dsb_theme_classic.R
+++ R/dsb_theme_classic.R
@@ -1,7 +1,7 @@
 #' Apply an Epinion styled theme to your `ggplot2` graphics
 #'
 #' This function applies a theme consistent with the Epinion Corporate Visual Identity (CVI) to your `ggplot2` graphics. Add it to your `ggplot2` syntax for conistent visual outputs.
-#' @param legend a logical value indicating whether a legend is included. Defaults to `FALSE`
+#' @param legend a logical value indicating whether a legend is included. Defaults to `TRUE` (with `legend.position` defaulting to `"bottom"`)
 #' @param gridlines Include gridlines? Options are `"both"`, `"none"`, `"x"` (for vertical), and `"y"` (for horizontal). Default is `"both"`
 #' @param textcolor Color for text elements in `theme()` aesthetics. Must be either "black" or a named color from the Epinion color palette (see \link[repinion]{getcols_epi}), include only the color name, not the `"Epinion"` prefix. Default is "black"
 #' @return a CVI compliant plot
@@ -55,8 +55,8 @@ dsb_theme_classic <- function(legend = TRUE,
                    strip.text = element_text(colour = theme_textcolor,
                                              face = "plain",
                                              size = 12),
-                   strip.background = element_rect(color = getElement(dsb_colvec(), "DSB_LightGrey"),
-                                                   fill = scales::alpha(getElement(dsb_colvec(), "DSB_LightGrey"), 0.4)),
+                   strip.background = element_rect(color = getElement(dsb_colvec(), "DSB LightGrey"),
+                                                   fill = scales::alpha(getElement(dsb_colvec(), "DSB LightGrey"), 0.4)),
                    plot.title = element_text(colour = theme_textcolor,
                                              face = "bold",
                                              size = 18),
