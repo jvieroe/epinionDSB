15f597f2cbe634fa819111350e6beaa43dfae628
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-10-29 15:04:48 +0200

    new themes

--- R/dsb_theme_classic.R
+++ R/dsb_theme_classic.R
@@ -1,13 +1,13 @@
 #' Apply an Epinion styled theme to your `ggplot2` graphics
 #'
-#' This function applies a theme consistent with the Epinion Corporate Visual Identity (CVI) to your `ggplot2` graphics. Add it to your `ggplot2` syntax for conistent visual outputs.
+#' This function applies a theme consistent with the DSB Corporate Visual Identity (CVI) to your `ggplot2` graphics. Add it to your `ggplot2` syntax for conistent visual outputs.
 #' @param legend a logical value indicating whether a legend is included. Defaults to `TRUE` (with `legend.position` defaulting to `"bottom"`)
 #' @param gridlines Include gridlines? Options are `"both"`, `"none"`, `"x"` (for vertical), and `"y"` (for horizontal). Default is `"both"`
-#' @param textcolor Color for text elements in `theme()` aesthetics. Must be either "black" or a named color from the Epinion color palette (see \link[repinion]{getcols_epi}), include only the color name, not the `"Epinion"` prefix. Default is "black"
+#' @param textcolor Color for text elements in `theme()` options. Must be either "black" or a named color from the DSB color palette (see \link[epinionDSB]{dsb_colvec}), include only the color name, not the `"Epinion"` prefix. Default is "black"
 #' @return a CVI compliant plot
 #' @examples
 #' library(tidyverse)
-#' ggplot(mtcars, aes(x = wt, y = mpg, color = factor(am))) +
+#' ggplot(mtcars, aes(x = wt, y = mpg)) +
 #' geom_point() +
 #' dsbtheme_classic()
 #' @author Jeppe Vierø
