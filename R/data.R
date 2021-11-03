c716a4103f1d748dfcae88799578599a3168f047
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-03 14:35:53 +0100

    data documentation

---
+++ R/data.R
@@ -0,0 +1,25 @@
+#' DSB colors
+#'
+#' A named vector with DSB colors in HEX codes
+#'
+#' @format A `list` with 4 elements
+"dsb_cols"
+
+
+#' Discrete DSB color palettes
+#'
+#' A dataset containing the location of 10,000 US cities
+#'
+#' @format A `list` with 1 element
+"dsbpal_d"
+
+
+#' Continuous DSB color palettes
+#'
+#' A spatial dataset containing
+#'
+#' @format A `list` with 4 elements
+"dsbpal_c"
+
+
+
